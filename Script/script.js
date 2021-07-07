1ac888aba7363f671b35fc5401a224a9b5b09c5b
Florencia Kania <62779015+flor-k@users.noreply.github.com>
2021-07-06 21:30:01 -0300

    version2

--- Script/script.js
+++ Script/script.js
@@ -1,4 +1,4 @@
-var Ingredientes=["Fideos", "Salsa", "Huevo"];
+var Ingredientes=["Fideos", "Salsa", "Huevo","Pan","Queso"];
 var IngTengo = [];
 var PosibComi = [];
 var Comidas={
@@ -15,32 +15,71 @@ var Comidas={
             Ingredientes[0],
             Ingredientes[2]
             ]
-        }
+        },
+    "Huevos revueltos con tostadas":{
+        "Tiempo": 20,
+        "Ingre":[
+            Ingredientes[3],
+            Ingredientes[2]
+            ]
+        },
+    "Tostadas de queso":{
+        "Tiempo": 15,
+        "Ingre":[
+            Ingredientes[3],
+            Ingredientes[4]
+        ]
+    }
+
 };
 
-var tiempoVal
+var tiempoVal;
+var tiempoComida;
 
 function getInputValue(){
     tiempoVal = document.getElementById("time").value;
+    document.getElementById("minutos-tiempo").style.backgroundColor="rgb(194, 140, 95)";
+
 };
 
 
 document.getElementById("fideos").onclick = function(){
     IngTengo.push(Ingredientes[0]);
+    document.getElementById("fideos").style.backgroundColor="rgb(194, 140, 95)";
 };
 
 document.getElementById("salsa").onclick = function(){
     IngTengo.push(Ingredientes[1]);
+    document.getElementById("salsa").style.backgroundColor="rgb(194, 140, 95)";
 };
 
 document.getElementById("huevo").onclick = function(){
     IngTengo.push(Ingredientes[2]);
+    document.getElementById("huevo").style.backgroundColor="rgb(194, 140, 95)";
+};
+
+document.getElementById("pan").onclick = function(){
+    IngTengo.push(Ingredientes[3]);
+    document.getElementById("pan").style.backgroundColor="rgb(194, 140, 95)";
+};
+
+document.getElementById("queso").onclick = function(){
+    IngTengo.push(Ingredientes[4]);
+    document.getElementById("queso").style.backgroundColor="rgb(194, 140, 95)";
 };
 
 document.getElementById("choose").onclick = function(){
+    var rand = Math.floor(Math.random() * PosibComi.length);
+
+    function Resultado(){
+        document.getElementById("comidas").innerHTML = (IngTengo);
+        document.getElementById("minutes").innerHTML = ("Te va a tomar " + tiempoVal + " minutos.");
+        document.getElementById("food").innerHTML = (PosibComi[rand]);
+    };
+
     for (var i in Comidas){
+        var cont = 0;
         for (var j in Comidas[i].Ingre){ 
-            var cont = 0;
             if(IngTengo.includes(Comidas[i]["Ingre"][j])){
                 cont += 1;
             } else {
@@ -48,14 +87,16 @@ document.getElementById("choose").onclick = function(){
                 break;
             };
         };
+        
         if ( cont != 0 && Comidas[i].Tiempo <= tiempoVal){
+            tiempoComida = Comidas[i].Tiempo;
             PosibComi.push(i);
-        };
+            };
+        
     };
 
-    var rand = Math.floor(Math.random() * PosibComi.length);
-    document.getElementById("minutes").innerHTML = (tiempoVal);
-    document.getElementById("food").innerHTML = (PosibComi[rand]);
-
+    if (PosibComi.length === 0){
+        PosibComi.push("¡Rappi!");
+    }
+    Resultado();
 };
-
